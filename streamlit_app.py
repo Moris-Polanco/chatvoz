0ef39f364aebd028f1cf4b1c451c168356f8ed15
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-10 20:30:51 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import openai
 import streamlit as st
-from gtts import gTTS 
 import os
 
 # Autenticación de OpenAI (oculta la clave en una variable de entorno)
@@ -9,7 +8,7 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def generate_text(prompt):
     completions = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="text-davinci-002",
         prompt=prompt,
         max_tokens=1024,
         n=1,
@@ -20,18 +19,17 @@ def generate_text(prompt):
     message = completions.choices[0].text
     return message.strip()
 
-def speak(text):
-    tts = gTTS(text=text, lang='es')
-    tts.save("response.mp3")
-    os.system("start response.mp3")
-
 def chatbot():
-    st.title("Chatbot")
-    message = st.text_input("Enter your message:")
-    if message:
-        response = generate_text(message)
-        st.success(response)
-        speak(response)
+    st.set_title("Chatbot")
+    previous_message = None
+    while True:
+        message = st.text_input("Enter your message:", value=previous_message)
+        if message:
+            previous_message = message
+            response = generate_text(message)
+            st.success(response)
+            if "exit" in message.lower():
+                break
 
 if __name__ == "__main__":
     chatbot()
